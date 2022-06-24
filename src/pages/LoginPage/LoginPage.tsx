2d21ed405be3bc60f08b1ee60161cc5d32a895ab
Dima Sotskiy <sotskiydr@gmail.com>
2022-06-24 10:58:08 +0300

    add auth pages

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,138 @@
-import LoginForm from "../../components/LoginForm/LoginForm";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {useLoginUserMutation} from "../../services/ReduxService";
+import {useState} from "react";
+import authBackground from '../../images/1150365296-huge.jpg';
 
-
-const LoginPage = () => {
-    return <LoginForm />
+function Copyright(props: any) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+          {/*{'Copyright © '}*/}
+          {/*<Link color="inherit" href="https://mui.com/">*/}
+          {/*    Your Website*/}
+          {/*</Link>{' '}*/}
+          {/*{new Date().getFullYear()}*/}
+          {/*{'.'}*/}
+          Created by people who have nothing to lose :)
+      </Typography>
+    );
 }
 
-export default LoginPage;
+const theme = createTheme();
+
+export default function SignInSide() {
+    const [loginUser, {}] = useLoginUserMutation()
+    const [formState, setFormState] = useState({
+        email: '',
+        password: ''
+    });
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const {name, value} = e.currentTarget
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+    }
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        await loginUser(formState)
+    };
+
+    return (
+      <ThemeProvider theme={theme}>
+          <Grid container component="main" sx={{ height: '100vh' }}>
+              <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                  <Box
+                    sx={{
+                        my: 8,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                  >
+                      <Typography sx={{ mb: 5}} component="h1" variant="h5">
+                          Welcome back to the English Easy space
+                      </Typography>
+                      <Typography component="h1" variant="h4">
+                          Sign in
+                      </Typography>
+                      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                          <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            onChange={handleChange}
+                            autoFocus
+                          />
+                          <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={handleChange}
+                          />
+                          <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                          >
+                              Sign In
+                          </Button>
+                          <Grid container>
+                              <Grid item xs>
+                                  <Link href="#" variant="body2">
+                                      Forgot password?
+                                  </Link>
+                              </Grid>
+                              <Grid item>
+                                  <Link href="/register" variant="body2">
+                                      {"Don't have an account? Sign Up"}
+                                  </Link>
+                              </Grid>
+                          </Grid>
+                          <Copyright sx={{ mt: 5 }} />
+                      </Box>
+                  </Box>
+              </Grid>
+              <CssBaseline />
+              <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
+                sx={{
+                    backgroundImage: `url(${authBackground})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                      t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+              />
+          </Grid>
+      </ThemeProvider>
+    );
+}
