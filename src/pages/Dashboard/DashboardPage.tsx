4afb1c3a5d82a2648fb1f846ca6f758204455baa
shvec-es <kotova.es.1510@gmail.com>
2022-07-16 14:56:05 +0300

    add dashboard style

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -1,49 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import s from "./DashboardPage.module.css";
+import s from "./DashboardPage.module.scss";
 import { Modal, AddNewWords } from "../../components";
-import {useAppDispatch, useAppSelector} from '../../store/hooks/redux';
-import { changeStateModal } from '../../store/reducers/ActionCreators';
+import { useAppDispatch, useAppSelector } from "../../store/hooks/redux";
+import { changeStateModal } from "../../store/reducers/ActionCreators";
 
 const DashboardPage: React.FC = () => {
-  // const [active, setActive] = useState<boolean>(false);
   const dispatch = useAppDispatch();
-  const { isModalOpenAddWord } = useAppSelector(state => state.ModalSlice);
+  const { isModalOpenAddWord } = useAppSelector((state) => state.ModalSlice);
 
   return (
     <section>
-      <ul className={s.list}>
-        <li className={s.item}>
-          <Link to="my_vocabulary">My vocabulary</Link>
-        </li>
-        <li className={s.item}>
-          <Link to="all_words">All words</Link>
-        </li>
-        <li className={s.item}>
-          <Link to="translator">Translator</Link>
-        </li>
-        <li className={s.item}>
-          <Link to="rules">Rules</Link>
-        </li>
-        <li className={s.item}>
-          <Link to="top_users">Top users</Link>
-        </li>
-        <li className={s.item}>
-          <Link to="chat">Chat</Link>
-        </li>
-      </ul>
-      <button
-        className={s.button}
-        type="button"
-        onClick={() => dispatch(changeStateModal(true, 'add'))}
-      >
-        Add New Words
-      </button>
-      {isModalOpenAddWord && (
-        <Modal action='add'>
-          <AddNewWords/>
-        </Modal>
-      )}
+      <div className={s.wrap}>
+        <div className={s.item}>
+          <div className={s["item-wrap"]}>
+            <Link className={s.link} to="my_vocabulary"><h3 className={s.title} >My vocabulary</h3></Link>
+          </div>
+        </div>
+        <div className={s.item}>
+          <div className={s["item-wrap"]}>
+            <Link className={s.link} to="all_words"><h3 className={s.title} >All words</h3></Link>
+          </div>
+        </div>
+        <div className={s.item}>
+          <div className={s["item-wrap"]}>
+            <Link className={s.link} to="translator"><h3 className={s.title} >Translator</h3></Link>
+          </div>
+        </div>
+        <div className={s.item}>
+          <div className={s["item-wrap"]}>
+            <Link className={s.link} to="rules"><h3 className={s.title} >Rules</h3></Link>
+          </div>
+        </div>
+        <div className={s.item}>
+          <div className={s["item-wrap"]}>
+            <Link className={s.link} to="top_users"><h3 className={s.title} >Top users</h3></Link>
+          </div>
+        </div>
+        <div className={s.item}>
+          <div className={s["item-wrap"]}>
+            <Link className={s.link} to="chat"><h3 className={s.title} >Chat</h3></Link>
+          </div>
+        </div>
+        <div className={s.item}>
+          <div className={s["item-wrap"]}>
+            <button
+              className={s.link}
+              type="button"
+              onClick={() => dispatch(changeStateModal(true, "add"))}
+            >
+              <h3 className={s.title} >Add New Words</h3>
+            </button>
+            {isModalOpenAddWord && (
+              <Modal action="add">
+                <AddNewWords />
+              </Modal>
+            )}
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
